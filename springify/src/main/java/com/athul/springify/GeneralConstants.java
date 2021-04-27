f92f92d6c0ef73bd4bdf9e5b7579e7152dfe47c8
Athul-Krishna <athulkrishnaks1998@gmail.com>
2021-04-27 15:48:47 +0530

    Add ec2Instance public url to general constants

--- springify/src/main/java/com/athul/springify/GeneralConstants.java
+++ springify/src/main/java/com/athul/springify/GeneralConstants.java
@@ -4,4 +4,5 @@ public class GeneralConstants {
     public static final String adminEmail = "athulkrishnaks1998@gmail.com";
     public static final String adminFirstName = "Athul";
     public static final String adminLastName = "Krishna";
+    public static final String ec2InstancePublicUrl = "http://ec2-65-2-69-99.ap-south-1.compute.amazonaws.com:8080";
 }
--- springify/src/main/java/com/athul/springify/shared/AmazonSES.java
+++ springify/src/main/java/com/athul/springify/shared/AmazonSES.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AmazonSES {
+    final String PUBLIC_URL = GeneralConstants.ec2InstancePublicUrl;
     final String FROM = GeneralConstants.adminEmail;
     final String SUBJECT = "One last step to complete your registration";
     final String PASSWORD_RESET_SUBJECT = "Password reset request";
@@ -17,21 +18,21 @@ public class AmazonSES {
     final String HTMLBODY = "<h1>Please verify your email address</h1>"
             + "<p>Thank you for registering with our mobile app. To complete registration process and be able to login,"
             + " click on the following link: "
-            + "<a href='http://ec2-13-233-159-51.ap-south-1.compute.amazonaws.com:8080/verification-service/email-verification.html?token=$tokenValue'>"
+            + "<a href='$publicUrl/verification-service/email-verification.html?token=$tokenValue'>"
             + "Final step to complete your registration" + "</a><br/><br/>"
             + "Thank you! And we are waiting for you inside!";
 
     final String TEXTBODY = "Please verify your email address. "
             + "Thank you for registering with our mobile app. To complete registration process and be able to login,"
             + " open the following URL in your browser window: "
-            + " http://ec2-13-233-159-51.ap-south-1.compute.amazonaws.com:8080/verification-service/email-verification.html?token=$tokenValue"
+            + " $publicUrl/verification-service/email-verification.html?token=$tokenValue"
             + " Thank you! And we are waiting for you inside!";
 
     final String PASSWORD_RESET_HTMLBODY = "<h1>A request to reset your password</h1>"
             + "<p>Hi, $firstName!</p> "
             + "<p>Someone has requested to reset your password with our project. If it were not you, please ignore it."
             + " Otherwise please click on the link below to set a new password: "
-            + "<a href='http://ec2-13-233-159-51.ap-south-1.compute.amazonaws.com:8080/verification-service/password-reset.html?token=$tokenValue'>"
+            + "<a href='$publicUrl/verification-service/password-reset.html?token=$tokenValue'>"
             + " Click this link to reset password" + "</a><br/><br/>"
             + "Thank you!";
 
@@ -39,13 +40,15 @@ public class AmazonSES {
             + "Hi, $firstName! "
             + "Someone has requested to reset your password with our project. If it were not you, please ignore it."
             + " Otherwise please open the link below in your browser window to set a new password: "
-            + " http://ec2-13-233-159-51.ap-south-1.compute.amazonaws.com:8080/verification-service/password-reset.html?token=$tokenValue"
+            + " $publicUrl/verification-service/password-reset.html?token=$tokenValue"
             + " Thank you!";
 
     public void verifyEmail(UserDto userDto) {
         AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard().withRegion(Regions.AP_SOUTH_1).build();
         String htmlBodyWithToken = HTMLBODY.replace("$tokenValue", userDto.getEmailVerificationToken());
         String textBodyWithToken = TEXTBODY.replace("$tokenValue", userDto.getEmailVerificationToken());
+        htmlBodyWithToken = htmlBodyWithToken.replace("$publicUrl", PUBLIC_URL);
+        textBodyWithToken = textBodyWithToken.replace("$publicUrl", PUBLIC_URL);
 
         SendEmailRequest request = new SendEmailRequest()
                 .withDestination(new Destination().withToAddresses(userDto.getEmail()))
@@ -65,8 +68,10 @@ public class AmazonSES {
         AmazonSimpleEmailService client = AmazonSimpleEmailServiceClientBuilder.standard().withRegion(Regions.AP_SOUTH_1).build();
         String htmlBodyWithToken = PASSWORD_RESET_HTMLBODY.replace("$tokenValue", token);
         htmlBodyWithToken = htmlBodyWithToken.replace("$firstName", firstName);
+        htmlBodyWithToken = htmlBodyWithToken.replace("$publicUrl", PUBLIC_URL);
         String textBodyWithToken = PASSWORD_RESET_TEXTBODY.replace("$tokenValue", token);
         textBodyWithToken = textBodyWithToken.replace("$firstName", firstName);
+        textBodyWithToken = textBodyWithToken.replace("$publicUrl", PUBLIC_URL);
 
         SendEmailRequest request = new SendEmailRequest()
                 .withDestination(new Destination().withToAddresses(email))
